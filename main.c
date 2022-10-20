b4c4b38ebe74b9fae550380a52de1e2f6b8d9adc
rem59570@gmail.com <rem59570@gmail.com>
2022-10-20 11:13:14 +0200

    PVE

--- main.c
+++ main.c
@@ -27,12 +27,10 @@ void pvp(){
             }else{
                 printf("La case est déjà prise\n");
             }
-            
             afficheTab(tab, taille_tab);
             fgets(choix, MAX_ENTREE, stdin);
             case_joue = atoi(choix) - 1;
             is_numeric = case_joue > 0 && case_joue < taille_tab * taille_tab -2;
-
         }
     }
     system("clear");
@@ -43,7 +41,63 @@ void pvp(){
     }else{
         printf("C'est une égalité\n");
     }
-    
+}
+
+void pve(){
+    int taille_tab = 25;
+    char* tab = malloc(sizeof(char) * taille_tab);
+    initMap(tab, taille_tab);
+    int pions[] = {'X', 'O'};
+    srand(time(NULL));
+    int tourJoueur = rand()%2;
+    int case_joue = 0;
+    int tour = -1;
+    char choix[512] = ""; 
+
+    while (!partie_fini(tab, case_joue, pions[tour%2], taille_tab, tour)){
+        ++tour;
+        system("clear");
+        afficheTab(tab, taille_tab);
+        
+        if (tour%2 == tourJoueur){
+
+            printf("C'est au tour du joueur %d\n", tour%2 + 1);
+            fgets(choix, MAX_ENTREE, stdin);
+            case_joue = atoi(choix) - 1;
+            int is_numeric = case_joue > 0 && case_joue < taille_tab * taille_tab -2;
+            while (poserCase(case_joue, tab, pions[tour%2]) && !is_numeric){
+                system("clear");
+                if (!is_numeric){
+                    printf("La saisie n'est pas valide\n");
+                }else{
+                    printf("La case est déjà prise\n");
+                }
+                afficheTab(tab, taille_tab);
+                fgets(choix, MAX_ENTREE, stdin);
+                case_joue = atoi(choix) - 1;
+                is_numeric = case_joue > 0 && case_joue < taille_tab * taille_tab -2;
+            }
+        }else{
+            //automatique
+            printf("C'est au tour de l'ordi\n");
+            int n = rand()%(taille_tab*taille_tab);
+            case_joue = n;
+            while (poserCase(case_joue, tab, pions[tour%2])){
+                system("clear");
+                n = rand()%10;
+                case_joue = n;
+                afficheTab(tab, taille_tab);
+            }
+        }
+    }
+    system("clear");
+
+    afficheTab(tab, taille_tab);
+    if (partie_fini(tab, case_joue, pions[tour%2], taille_tab, tour) == -1){
+        printf("Le joueur %d a gagné\n", tour%2 + 1);
+    }else{
+        printf("C'est une égalité\n");
+    }
 }
 
 
@@ -58,10 +112,9 @@ int main(void){
     }while (!strstr(choix, "1")  && !strstr(choix, "2")  && !strstr(choix, "3") );
 
     if (strstr(choix, "1")){
-        printf("PVP\n");
         pvp();
     }else if (strstr(choix, "2")){
-        printf("PVE\n");
+        pve();
     }else{
         printf("ORDI vs ORDI\n");
     }
--- main.h
+++ main.h
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <time.h>
 
 #include "affichage.h"
 #include "action.h"
